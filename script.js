0d4cd84fa50ccb64bfe5c330d02c7976bc6046cc
sophiazhurkova <kratovo06@gmail.com>
2025-11-26 19:20:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const animals = [
+    { name: "Раф", price: "Парень я активный: люблю долгие прогулки и подвижные игры. Правила жизни в доме знаю на отлично, надеюсь, что скоро найду любящего хозяина! Скорее приезжайте знакомиться, всех жду!", img: "./images/roses.jpeg" },
+    { name: "Кася", price: "У меня очень красивая и улыбчивая мордочка, шикарные уши и добрые глазки! Моя мечта — обрести дом и любящего хозяина, которому я смогу раскрыть своё сердце.", img: "./images/пионовидные-розы.jpg" },
+    { name: "Валдай", price: "Я ласковый парень, очень люблю внимание от людей — когда меня чешут за ушком и играются со мной удочкой.", img: "./images/пионы.jpg" },
+    { name: "Пэнни", price: "Я красивая смешнуля с большими-большими глазами. Во мне течет кровь настоящей аристократки, ведь я породистая Британка.", img: "./images/хризантемы.jpg" },
+    { name: "Джо", price: "Ищу дом со скрипкой", img: "./images/тюльпан.jpg" },
+     { name: "Джери", price: "Просто хороший друг", img: "./images/тюльпан.jpg" },
+];
+
+function renderAnimals() {
+    const container = document.getElementById("animal-list");
+    if (!container) return;
+
+    animals.forEach(animal => {
+        const card = `
+        <div class="col-md-4 mb-4 d-flex justify-content-center">
+            <div class="animal-card">
+                <img src="${animal.img}" class="img-fluid rounded mb-2">
+                <h5>${animal.name}</h5>
+                <p>${animal.price}</p>
+                <p class="card-text">Выбор <input class="w-25" type="checkbox" name="choice"></p>
+            </div>
+        </div>`;
+        container.innerHTML += card;
+    });
+}
+
+renderAnimals();
